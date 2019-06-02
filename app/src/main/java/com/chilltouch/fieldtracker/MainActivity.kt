b1171e2a1ee40492297def59612a440e839438cd
ngavrilov <n.gavrilov@onladv.com>
2019-06-02 10:59:28 +0300

    - add navigation drawer

--- app/src/main/java/com/chilltouch/fieldtracker/MainActivity.kt
+++ app/src/main/java/com/chilltouch/fieldtracker/MainActivity.kt
@@ -6,15 +6,23 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.location.LocationManager
 import android.support.v4.content.ContextCompat
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     var locationManager : LocationManager? = null
     var locationListener : CordinateListener? = null
 
+    var drawer : DrawerLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.createMenus()
 
         if (locationListener == null) {
             locationListener = CordinateListener()
@@ -25,6 +33,25 @@ class MainActivity : AppCompatActivity() {
                     ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)) != PackageManager.PERMISSION_GRANTED) {
         } else {
             locationManager!!.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60, 500f, locationListener)
+        }
+    }
+
+    private fun createMenus() {
+        var toolbar : Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById<DrawerLayout?>(R.id.activity_main)
+        var toggle : ActionBarDrawerToggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer?.addDrawerListener(toggle)
+
+        toggle.syncState()
+    }
+
+    override fun onBackPressed() {
+        if (drawer!!.isDrawerOpen(GravityCompat.START)) {
+            drawer?.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
 }
