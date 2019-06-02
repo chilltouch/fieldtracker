b9ec4f7b24a359729d90d49b688ac960a1a7e060
ngavrilov <n.gavrilov@onladv.com>
2019-06-02 11:35:02 +0300

    - add Home and Settings fragments

--- app/src/main/java/com/chilltouch/fieldtracker/MainActivity.kt
+++ app/src/main/java/com/chilltouch/fieldtracker/MainActivity.kt
@@ -5,13 +5,16 @@ import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.location.LocationManager
+import android.provider.Settings
+import android.support.design.widget.NavigationView
 import android.support.v4.content.ContextCompat
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     var locationManager : LocationManager? = null
     var locationListener : CordinateListener? = null
@@ -22,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        this.createMenus()
+        this.createMenus(savedInstanceState)
 
         if (locationListener == null) {
             locationListener = CordinateListener()
@@ -36,15 +39,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun createMenus() {
+    private fun createMenus(savedInstanceState: Bundle?) {
         var toolbar : Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
         drawer = findViewById<DrawerLayout?>(R.id.activity_main)
+
+        var navigationView : NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
         var toggle : ActionBarDrawerToggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer?.addDrawerListener(toggle)
 
         toggle.syncState()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+            navigationView.setCheckedItem(R.id.fragment_container)
+        }
     }
 
     override fun onBackPressed() {
@@ -54,4 +66,18 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         }
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.nav_home) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment())
+                .addToBackStack(null).commit()
+        } else if (item.itemId == R.id.nav_settings) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, SettingsFragment())
+                .addToBackStack(null).commit()
+        }
+
+        drawer?.closeDrawer(GravityCompat.START)
+        return true
+    }
+
 }
